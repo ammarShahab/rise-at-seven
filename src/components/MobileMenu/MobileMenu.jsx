9538924ca8ec4c6fdf086df313d26fae51a4123f
ammarShahab <ashahab007@gmail.com>
2026-05-12 22:42:26 +0600

    Remove extra mobile menu close button

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -44,7 +44,7 @@ function MobileMenu({ isOpen, onClose }) {
               onClick={onClose}
               aria-label="Close menu"
             >
-              <svg
+              {/* <svg
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
@@ -56,7 +56,7 @@ function MobileMenu({ isOpen, onClose }) {
               >
                 <line x1="18" y1="6" x2="6" y2="18" />
                 <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
+              </svg> */}
             </button>
           </div>
 
